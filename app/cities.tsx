a35c7168bd13049e2de13ef79bd5a2ee21a91a25
Wilson Carvalho <eng.wilson.c@gmail.com>
2025-01-28 11:09:36 -0300

    chore: cities list added

--- app/cities.tsx
+++ app/cities.tsx
@@ -1,7 +1,40 @@
-import { View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import citiesData from "../data/cities.json";
 
 const Cities = () => {
-  return <View></View>;
+  console.log(citiesData);
+
+  return (
+    <LinearGradient colors={["#00457d", "#05051f"]} style={style.container}>
+      {citiesData.map((city) => (
+        <View style={style.listItem}>
+          <Text style={style.cityName}>{city.city}</Text>
+        </View>
+      ))}
+    </LinearGradient>
+  );
 };
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    gap: 16,
+  },
+  listItem: {
+    height: 63,
+    width: "100%",
+    backgroundColor: "rgba(255,255,255, 0.15)",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 16,
+  },
+  cityName: {
+    color: "#fff",
+    fontSize: 16,
+    fontFamily: "Montserrat_500Medium",
+  },
+});
 
 export default Cities;
