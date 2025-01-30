0afae934f6ecfd932bb05c93606b4db72bf884e1
Wilson Carvalho <eng.wilson.c@gmail.com>
2025-01-30 10:34:55 -0300

    chore: add api call

--- app/[cityName].tsx
+++ app/[cityName].tsx
@@ -1,12 +1,57 @@
-import { View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import { LinearGradient } from "expo-linear-gradient";
 
 const CityDetails = () => {
   const searchParams = useLocalSearchParams();
+  const [cityDetails, setCityDetails] = useState(null);
 
-  console.log(searchParams);
+  const handleData = async () => {
+    try {
+      const response = await fetch("https://climapp-api.vercel.app/api");
+      const responseJSON = await response.json();
 
-  return <View />;
+      const city = responseJSON.find(
+        (cityData) => cityData.city === searchParams.cityName
+      );
+
+      setCityDetails(city);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    handleData();
+  }, []);
+
+  return (
+    <LinearGradient colors={["#00457d", "#05051f"]} style={style.container}>
+      <View>
+        <Text style={style.headerTitle}>{cityDetails.city}</Text>
+      </View>
+    </LinearGradient>
+  );
 };
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 40,
+  },
+  headerContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  headerTitle: {
+    color: "#fff",
+    fontSize: 20,
+    fontFamily: "Montserrat_500Medium",
+    textAlign: "center",
+  },
+});
 
 export default CityDetails;
